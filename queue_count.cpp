046a63b8a28e73214991f4422496cc87f967892b
Vijak Khajornritdacha <vijak13019@hotmail.com>
2023-08-31 15:12:28 +0700

    add probs

---
+++ queue_count.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+size_t qcount(queue<int> q, int k) {
+ size_t cnt = 0;
+ while (!q.empty()) {
+    int x = q.front();
+    q.pop();
+    cnt += (k == x);
+ }
+ return cnt;
+}
+int main() {
+ // for faster cin, cout
+ ios_base::sync_with_stdio(false); cin.tie(0);
+ int n,k;
+ cin >> n >> k;
+ queue<int> q;
+ for (int i = 0;i < n;i++) {
+ int a;
+ cin >> a;
+ q.push(a);
+ }
+ cout << qcount(q,k) << endl;
+}
